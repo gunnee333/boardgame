b709de14242952731354ee7c4e01f80db06a4c2d
gh.lee <gunheelee3394@gmail.com>
2025-05-04 23:54:52 +0900

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,33 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ['react', '@typescript-eslint'],
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
   },
   rules: {
-    // 원하는 규칙 추가 가능
-    "react/react-in-jsx-scope": "off", // Next.js나 CRA에서는 React import가 필요 없음
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+        allowTaggedTemplates: true,
+      },
+    ],
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
 };
